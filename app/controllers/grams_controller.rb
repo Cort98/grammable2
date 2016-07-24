3493521fcaf3bf1eab79bd1af77f63a02776f34a
Cort Ferguson <98cort@gmail.com>
2016-07-24 19:52:32 +0000

    connect gram to user

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,7 @@
 class GramsController < ApplicationController
-  def index
+  before_action :authenticate_user!, only: [:new, :create]
 
+  def index
   end
 
   def new
@@ -8,8 +9,8 @@ class GramsController < ApplicationController
   end
 
   def create
-    @gram = Gram.create(gram_params)
-    
+    @gram = current_user.grams.create(gram_params)
+
     if @gram.valid?
       redirect_to root_path
     else
